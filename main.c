85274cfbf84781e85a3a6344e405e39e44005e69
AtalGuzman <adolfo.guzman@usach.cl>
2015-11-22 12:43:54 -0300

    Se agrega la función para elminar

--- main.c
+++ main.c
@@ -8,6 +8,15 @@ typedef struct matriz{
 	int ** m;
 }M;
 
+
+M* crearMatriz(int dimension,int lineaN,int lineaM, int** columna){
+	M* matriz = malloc(sizeof(M));
+	matriz->f=lineaN;
+	matriz->c= lineaM;
+	matriz->m= columna;
+	return matriz;
+}
+
 M* leerMatriz(char* entrada){
 	FILE* archivoEntrada = fopen(entrada,"r");
 	int lineaN,lineaM;
@@ -16,7 +25,7 @@ M* leerMatriz(char* entrada){
 	int j=0;
 	int temporal;
 	fscanf(archivoEntrada,"%d\n%d\n",&lineaN,&lineaM);
-	dim = lineaN*lineaM;
+	dim = lineaN*lineaM;	
 	int* fila = malloc(dim*sizeof(int));
 	int** columna = malloc(lineaN*sizeof(int*));
 
@@ -28,11 +37,7 @@ M* leerMatriz(char* entrada){
 		}	
 		i++;
 	}
-	
-	M* matriz = malloc(sizeof(M));
-	matriz->f=lineaN;
-	matriz->c= lineaM;
-	matriz->m= columna;
+	M* matriz = crearMatriz(dim,lineaN,lineaM,columna);
 	fclose(archivoEntrada);
 	return matriz;
 }
@@ -52,20 +57,42 @@ void escribirMatriz(M* matriz, char* nombre){
 
 void mostrarMatriz(M* matriz){
 	int i,j;
-	for(i=0;i<matriz->f;i++){
-		for(j=0;j<matriz->c;j++){
-			printf("%d ",matriz->m[i][j]);
-		}	
-		printf("\n");
+	if(matriz==NULL){
+		printf("[NULL]\n");	
+	}
+	else{
+		for(i=0;i<matriz->f;i++){
+			for(j=0;j<matriz->c;j++){
+				printf("%d ",matriz->m[i][j]);
+			}	
+			printf("\n");
+		}
 	}
 }
-main(int argc, char* argv[]){
 
+void borrarMatriz(M** matriz){
+	int i;
+	if(*matriz!=NULL){
+		for(i =0; i<(*matriz)->f;i++){
+			(*matriz)->m[i]=NULL;
+		}
+		free(*matriz);
+		*matriz=NULL;
+	}
+	else{
+		printf("[NULL]\n");
+	}
+}
+
+int main(int argc, char* argv[]){
 	M* matriz=leerMatriz(argv[1]);
-
-	escribirMatriz(matriz,"G");
-
-	M* matriz2=leerMatriz("G");
-	mostrarMatriz(matriz2);
+	printf("FILA: %d, Columna %d\n",matriz->f,matriz->c);
+	mostrarMatriz(matriz);
+	borrarMatriz(&matriz);
+	mostrarMatriz(matriz);	
+	//M* matriz2 = NULL;
+	//M* matriz2=leerMatriz("G");
+	//borrarMatriz(matriz2);
+	//mostrarMatriz(matriz2);
 	return 0;
 }
