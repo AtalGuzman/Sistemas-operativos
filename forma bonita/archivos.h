785c3e77d2d9682a63ec4733a15045856684bec5
AtalGuzman <adolfo.guzman@usach.cl>
2015-11-22 14:12:23 -0300

    Se cambia el tipo de dato M a Mat, para evitar ambiguedades

--- forma bonita/archivos.h
+++ forma bonita/archivos.h
@@ -1,8 +1,9 @@
 #ifndef __ARCHIVOS_H__
 #define __ARCHIVOS_H__
 
-M* leerMatriz(char* entrada);
-void escribirMatriz(M* matriz, char* nombre);
-void mostrarMatriz(M* matriz);
-
+Mat* leerMatriz(char* entrada);
+void escribirMatriz(Mat* matriz, char* nombre);
+void mostrarMatriz(Mat* matriz);
+void borrarMatriz(Mat** matriz);
+Mat* crearMatriz(int lineaN,int lineaM,int** columna);
 #endif
--- forma bonita/main.c
+++ forma bonita/main.c
@@ -5,13 +5,34 @@
 #include "archivos.h"
 #include "funciones.h"
 
-main(int argc, char* argv[]){
-
-	M* matriz=leerMatriz(argv[1]);
-
-	escribirMatriz(matriz,"G");
-
-	M* matriz2=leerMatriz("G");
-	mostrarMatriz(matriz2);
+int main(int argc, char* argv[]){
+	char buffer[255];
+	/*Se inicializan todas las matrices en valores nulos*/
+	Mat* A = crearMatriz(0,0,NULL);
+	Mat* B = crearMatriz(0,0,NULL);
+	Mat* C = crearMatriz(0,0,NULL);
+	Mat* D = crearMatriz(0,0,NULL);
+	Mat* E = crearMatriz(0,0,NULL);
+	Mat* F = crearMatriz(0,0,NULL);
+	Mat* G = crearMatriz(0,0,NULL);
+	Mat* H = crearMatriz(0,0,NULL);
+	Mat* I = crearMatriz(0,0,NULL);
+	Mat* J = crearMatriz(0,0,NULL);
+	Mat* K = crearMatriz(0,0,NULL);
+	Mat* L = crearMatriz(0,0,NULL);
+	Mat* M = crearMatriz(0,0,NULL);
+	Mat* N = crearMatriz(0,0,NULL);
+	Mat* O = crearMatriz(0,0,NULL);
+	Mat* P = crearMatriz(0,0,NULL);
+	Mat* Q = crearMatriz(0,0,NULL);
+	Mat* R = crearMatriz(0,0,NULL);
+	Mat* S = crearMatriz(0,0,NULL);
+	Mat* T = crearMatriz(0,0,NULL);
+	Mat* U = crearMatriz(0,0,NULL);
+	Mat* V = crearMatriz(0,0,NULL);
+	Mat* W = crearMatriz(0,0,NULL);
+	Mat* X = crearMatriz(0,0,NULL);
+	Mat* Y = crearMatriz(0,0,NULL);
+	Mat* Z = crearMatriz(0,0,NULL);
 	return 0;
 }
